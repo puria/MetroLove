38a165221f7889b5f1d87a61097330e491898f45
Puria Nafisi Azizi <puria.nafisi@axant.it>
2012-11-17 19:45:54 +0100

    Play method

--- src/org/sexmeter/android/Dashboard.java
+++ src/org/sexmeter/android/Dashboard.java
@@ -1,10 +1,17 @@
 package org.sexmeter.android;
 
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 
 @EActivity(R.layout.dashboard)
 public class Dashboard extends Activity {
+	
+	@Click
+	void play() {
+		startActivity(new Intent(this, MetroLove_.class));
+	}
 
 }
